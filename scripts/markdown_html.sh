f3441488b0951b4882aa8a7fad46cf2e7e01602c
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-08-24 11:23:20 +0200

    adjusted script to remove id's from headings

--- scripts/markdown_html.sh
+++ scripts/markdown_html.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-echo 'testing shell scripting'
+echo 'Converting markdown files to html...'
+
+pandoc -f markdown-auto_identifiers ../content/hooks.md -o ../content/hooks.html	
+
+
+echo 'Prettifying files...'
+prettier --write ../content/*.html
