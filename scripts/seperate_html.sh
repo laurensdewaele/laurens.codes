3b0bb02b01e623a542804bc484f697eed9e383c7
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-08-28 13:48:46 +0200

    restored old bash scripts

---
+++ scripts/seperate_html.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo 'Seperating the header from the content, so the header can be used on the index.html as well...'
+
+# Dus ik wil de datun + afbleeding + titel, en dit injecteren in de index.html. Plus linken naar de content.
+# De content zal de naam van de file zijn.
