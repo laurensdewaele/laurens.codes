3d0631bfaa5e29a64b99d9c7fe6f1c08f2d97695
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-08-29 09:52:09 +0200

    deleted old shell scripts

--- scripts/shell/seperate_html.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-echo 'Seperating the header from the content, so the header can be used on the index.html as well...'
-
-# Dus ik wil de datun + afbleeding + titel, en dit injecteren in de index.html. Plus linken naar de content.
-# De content zal de naam van de file zijn.
