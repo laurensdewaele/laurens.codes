6a9e9a4db4cf34c501161f3fca578b8342eecfa3
Laurens Dewaele <laurens.dewaele@gmail.com>
2019-08-29 10:45:35 +0200

    commit before testing image resizing

--- scripts/ready.js
+++ scripts/ready.js
@@ -2,6 +2,7 @@ const chalk = require('chalk');
 const fs = require('fs');
 const moment = require('moment');
 const prompts = require('prompts');
+const sharp = require('sharp');
 
 ready();
 
@@ -15,10 +16,13 @@ async function ready() {
     return;
   }
 
-  const createdDate = extractCreatedDate(file)
+  const createdDate = extractCreatedDate(file);
   const allImagesRe = /(\.\/images\/.*(?=\)))/g;
-  const fileAsText = fs.readFileSync(file, { encoding: 'UTF-8'})
-  const referencedImagesInPost = fileAsText.match(allImagesRe)
+  const fileAsText = fs.readFileSync(file, { encoding: 'UTF-8' });
+  const imagePaths = fileAsText.match(allImagesRe);
+  resizeImages(imagePaths)
+    .then(_ => chalk.green('Successfully resized referenced images'))
+    .catch(error => chalk.red(`Error in resizing images: ${error}`));
 }
 
 function promtFilename() {
@@ -36,4 +40,38 @@ function filenameExists(file) {
 function extractCreatedDate(file) {
   const { birthTime } = fs.statSync(file);
   return moment(birthTime).format('YYYY-MM-DD');
+}
+
+function addInfoToImagePaths(imagePaths) {
+  const descriptionRe = /^\.\/images\/(.*)\..{3,4}$/;
+  return imagePaths.map(path => {
+    const description = path.match(descriptionRe)[1];
+    const relativePathFromInsideScriptsFolder = `.${imagePath}`;
+    return {
+      path,
+      description,
+      relativePathFromInsideScriptsFolder
+    };
+  });
+}
+
+function resize(path, width, description) {
+  return sharp(path)
+    .resize(width)
+    .toFile(`../content_ready/images/${description}`);
+}
+
+function resizeImages(imagePaths, desktopWidth, mobileWidth) {
+  const resizeTasks = [];
+  const images = addInfoToImagePaths(imagePaths);
+  images.forEach(image => {
+    const { description, relativePathFromInsideScriptsFolder } = image;
+    resizeTasks.push(
+      resize(relativePathFromInsideScriptsFolder, desktopWidth, description)
+    );
+    resizeTasks.push(
+      resize(relativePathFromInsideScriptsFolder, mobileWidth, description)
+    );
+  });
+  return Promise.all(resizeTasks);
 }
